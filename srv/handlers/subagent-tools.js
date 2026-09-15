e6e855177d118f60860d5a685901d53ee387dc47
Daniel Hutzel <daniel.hutzel@sap.com>
2026-09-15 16:29:36 +0200

    Also truncate log output of response texts

--- srv/handlers/subagent-tools.js
+++ srv/handlers/subagent-tools.js
@@ -96,7 +96,7 @@ function createA2ATool(client, agentCard) {
           LOG.trace(`Raw results from ${subagent}`, inspect(result, { depth: null, colors: true }))
         let response = formatToolResult(extractResult(result))
         if (response)
-          LOG.info(`Got response from ${subagent}`, { messageId }, "\n\n" + response + "\n")
+          LOG.info(`Got response from ${subagent}`, { messageId }, "\n\n" + truncated(response) + "\n")
         return response
       } catch (err) {
         LOG.warn("Subagent tool error", { subagent, error: err.message })
